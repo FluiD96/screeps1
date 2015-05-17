bcc741c70fd2b40bec42e1e825cc12188ef70873
FluiD96 <FluiD96@mail.ru>
2015-05-17 16:36:40 +0400

    Create finisher.js

---
+++ finisher.js
@@ -0,0 +1,8 @@
+/*
+ * Module code goes here. Use 'module.exports' to export things:
+ * module.exports = 'a thing';
+ *
+ * You can import it from another modules like this:
+ * var mod = require('finisher'); // -> 'a thing'
+ */
+ Memory.finished=1;
