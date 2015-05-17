b52e3a2dc0048375733378f6e553cdc2a06b35a2
FluiD96 <FluiD96@mail.ru>
2015-05-17 16:38:05 +0400

    Create init.js

---
+++ init.js
@@ -0,0 +1,8 @@
+if (!Memory.init) {
+    // Initialization not done: do it
+    Memory.finished =1;
+    Memory.sid0 = 0;
+    Memory.sid1 = 0;
+    // Set the initialization flag
+    Memory.init = 1;
+}
